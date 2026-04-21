9adfb2160d151e31c10db3ec6506fe119055d04e
Mustafa Yalçınkaya <myalcinkaya136@gmail.com>
2026-04-22 01:02:35 +0300

    app tamamlandi

---
+++ OtherPratics/src/main/java/com/folksdev/secondhand/user/PalindromeNumberApp.java
@@ -0,0 +1,32 @@
+package com.folksdev.secondhand.user;
+
+import java.util.Scanner;
+
+public class PalindromeNumberApp {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Bir sayı giriniz: ");
+        int number = scanner.nextInt();
+
+        if (isPalindrome(number)) {
+            System.out.println(number + " bir palindrom sayıdır.");
+        } else {
+            System.out.println(number + " bir palindrom sayı değildir.");
+        }
+    }
+
+    static boolean isPalindrome(int number) {
+        int temp = number;
+        int reverseNumber = 0;
+        int lastNumber;
+
+        while (temp != 0) {
+            lastNumber = temp % 10;
+            reverseNumber = (reverseNumber * 10) + lastNumber;
+            temp /= 10;
+        }
+
+        return number == reverseNumber;
+    }
+}
