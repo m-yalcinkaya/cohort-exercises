c574adbba2a5debc82cf4d286aaabaea23539700
Mustafa Yalçınkaya <myalcinkaya136@gmail.com>
2026-04-22 12:03:12 +0300

    app tamamlandi

---
+++ OtherPratics/src/main/java/com/folksdev/secondhand/user/HackerrankOverrideMethodApp/Soccer.java
@@ -0,0 +1,13 @@
+package com.folksdev.secondhand.user.HackerrankOverrideMethodApp;
+
+class Soccer extends Sports{
+    @Override
+    String getName(){
+        return "Soccer Class";
+    }
+
+    @Override
+    void getNumberOfTeamMembers(){
+        System.out.println( "Each team has 11 players in " + getName() );
+    }
+}
